29a2db45180c5056990145136ae30c19bb2f597f
Ty Crawford <tygcrawford@gmail.com>
2024-04-16 20:59:48 -0400

    small changes to nvim and tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,9 @@
 require("kickstart")
+
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.keymap.set("n", "<CR>j", "o<Esc>k", { desc = "Create empty line above" })
+vim.keymap.set("n", "<CR>k", "O<Esc>j", { desc = "Create empty line below" })
