7bc87ce2037e8135932075baaa43be9e6ccd7d9d
Ty Crawford <tygcrawford@gmail.com>
2024-10-29 11:33:19 -0400

    updates to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,14 @@ vim.opt.shiftwidth = 2
 
 vim.keymap.set("n", "<CR>j", "o<Esc>k", { desc = "Create empty line above" })
 vim.keymap.set("n", "<CR>k", "O<Esc>j", { desc = "Create empty line below" })
+
+vim.filetype.add({
+	extension = {
+		zsh = "sh",
+		sh = "sh",
+	},
+	filename = {
+		[".zshrc"] = "sh",
+		[".zshenv"] = "sh",
+	},
+})
